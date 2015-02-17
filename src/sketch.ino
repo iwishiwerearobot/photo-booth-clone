0738853a3dbed8ed8110a495b3c05777f98a3db1
Pete Hinchcliffe <pete.hinchcliffe@googlemail.com>
2015-02-17 21:04:14 +0000

    fireIR function
    This function handles the bursts to the IR LED to trigger the cameras shutter.

--- src/sketch.ino
+++ src/sketch.ino
@@ -11,3 +11,23 @@ void setup()
 void loop()
 {
 }
+
+void fireIR()
+{
+  for(int i=0; i<16; i++)
+  {
+    digitalWrite(irLED, HIGH);
+    delayMicroseconds(11);
+    digitalWrite(irLED, LOW);
+    delayMicroseconds(11);
+  }
+  delayMicroseconds(7330);
+  for(int i=0; i<16; i++)
+  {
+    digitalWrite(irLED, HIGH);
+    delayMicroseconds(11);
+    digitalWrite(irLED, LOW);
+    delayMicroseconds(11);
+  }
+}
+
