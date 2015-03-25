025f88d43e4bbb7cb90220bc4d2c7fe4f48c2ee4
Pete Hinchcliffe <pete.hinchcliffe@googlemail.com>
2015-03-25 20:37:52 +0000

    remove the delay

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,18 +1,27 @@
-
 #define irLED 12
 #define button 2
+#define GREEN1 4
+#define GREEN2 5
+#define YELLOW1 6
+#define RED1 7
 
 void setup()
 {
   pinMode(button,INPUT);
   pinMode(irLED,OUTPUT);
+  pinMode(GREEN1,OUTPUT);
+  pinMode(GREEN2,OUTPUT);
+  pinMode(YELLOW1,OUTPUT);
+  pinMode(RED1,OUTPUT);
 }
 
 void loop()
 {
   if (digitalRead(button) == HIGH)
   {
+    //TURN ON ALL LEDS MINUS THE IRLED
     delay(2000);
+    //TURN OFF ALL LEDS MINUS THE IRLED
     startSeq();
   }
 }
@@ -21,8 +30,20 @@ void startSeq()
 {
   for(int i=0; i<4; i++)
   {
+   digitalWrite(GREEN1, HIGH);
+   delay(1333);
+   digitalWrite(GREEN2, HIGH);
+   delay(1333);
+   digitalWrite(YELLOW1, HIGH);
+   delay(1333);
+   digitalWrite(RED1, HIGH);
+   delay(1000);
    fireIR();
-   delay(5000);
+   digitalWrite(RED1, LOW);
+   digitalWrite(YELLOW1, LOW);
+   digitalWrite(GREEN2, LOW);
+   digitalWrite(GREEN1, LOW);
+   delay(500);
   }
 }
 
